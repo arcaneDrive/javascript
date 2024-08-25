88f09a5b39bb3ade5cf3ef9909e032d0a9af8b39
kweyaanton <kweyaanton@gmail.com>
2024-08-25 20:49:42 +0300

    commits

--- js/app.js
+++ js/app.js
@@ -80,6 +80,7 @@ const checkConfirmPassword = () => {
 
 // check if is valid email
 const isEmailValid = (email) => {
+    
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
 };
